3d6bd49d641b4c6c4bc3618a1b6e5fe5d0fa9ba8
yuyingm <yuying.mak@gmail.com>
2023-11-07 12:44:53 +0100

    ReadMe Updated
    - Updated readme with key information about what I'm trying to do as of the start of this project, and a project timeline.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # RedditWebScrape
 A test repository for a DA Portfolio project in scraping Reddit ethically / legally using Python, &amp; JSON
+
+Date Started:
+7 Nov, 2023
+
+Project Outline:
+I'd like to create a DA Portfolio piece where I scrape data from reddit to answer a research question.
+Probably, this research question will be related to inflation and cost of living from the r/sydney sub-reddit, where I am from.
+However, baby steps first and I'd like to just be able to work through:
+- python
+- json
+
+And then perform some analytics on it to change the numbers / what I find into actual informative knowledge using perhaps Tableau or some kind of data visualisation tools.
+
+I'd also like this to be a proof of use of GitHub, just to practice using it!
+
+Timeframe:
+I'd like to give myself a 2-3 month timeframe.
--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,22 +1,18 @@
 import requests
+import csv
+import time
 from bs4 import BeautifulSoup
 
-def scrape_subreddit(subreddit, keyword):
-    url = f"https://www.reddit.com/r/{subreddit}/search/?q={keyword}"
-    headers = {"User-Agent": "Your User Agent"}  # Add a user agent to mimic a web browser request
+url = "https://old.reddit.com/r/datascience"
+# Headers to mimic a browser visit
+headers = {'User-Agent': 'Mozilla/5.0'}
 
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(page.text, 'html.parser')
 
-    # Extract relevant information
-    posts = soup.find_all('div', class_='scrollerItem')
+domains = soup.find_all("span", class_="domain")
 
-    for post in posts:
-        title = post.find('h3').text
-        link = post.find('a')['href']
-        print(f"Title: {title}\nLink: {link}\n")
+for domain in domains:
+    if domain != "(self.datascience)":
+        continue
 
-if __name__ == "__main__":
-    subreddit_name = "your_subreddit_name"
-    search_keyword = "cost of living"
-    scrape_subreddit(subreddit_name, search_keyword)
+    print(domain.text)
